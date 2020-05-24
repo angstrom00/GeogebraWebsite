adb7e7a8535f9bbd63cc78f53fbd878497daf418
angstrom00 <angstrom1123@gmail.com>
2020-05-24 16:28:17 +0900

    for deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top'
   get 'top' => "home#top"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
