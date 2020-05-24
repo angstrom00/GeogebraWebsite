ec82ea93d6258bd7c6892df6f7a1aa7e1bf6de88
angstrom00 <angstrom1123@gmail.com>
2020-05-24 17:30:56 +0900

    change application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
-//= require jquery
-//= require jquery_ujs
+// = require jquery
+// = require jquery_ujs
 
 
 
